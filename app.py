c5ef1963f19db656d0fa869591926fd190b4f6ca
Nikolai124 <g4728281919@gmail.com>
2026-08-12 13:48:41 +0300

    обновил каталог
    Осталось сделать ползунок для фильтра цен, карточки товаров и накинуть дезайн

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+import json
+
+
 from flask import Flask
 from flask import render_template
 
-
+with open("products.json", "r", encoding="utf-8") as file:
+    product_list = json.load(file)
 app = Flask(__name__)
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def hello():
+    return render_template('index.html', products=product_list)
