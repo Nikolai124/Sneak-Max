3d13b24910f9fb541bda97d87ecab01fdd2f688c
Nikolai124 <g4728281919@gmail.com>
2026-08-16 03:44:05 +0300

    сделал запись из json в переменные и отправку на фронт

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ import json
 from flask import Flask
 from flask import render_template
 
+
 with open("products.json", "r", encoding="utf-8") as file:
     product_list = json.load(file)
+with open("quiz.json", "r", encoding="utf-8") as file:
+    quiz_list = json.load(file)
 app = Flask(__name__)
 @app.route('/')
-def hello():
-    return render_template('index.html', products=product_list)
+def main():
+    return render_template('index.html', products=product_list), render_template('index.html',quiz=quiz_list)
